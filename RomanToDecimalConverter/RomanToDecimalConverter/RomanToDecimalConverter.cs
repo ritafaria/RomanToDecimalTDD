b03ba873bde54fd8ecc896b80641248369e30dd3
ritafaria <rita.faria@live.com>
2016-09-06 01:39:05 -0700

    Stubbing RomanToDecimal converter
    - Added minimal imolementation for RomanToDecimal converter
    - Added reference in test project so that it compiles
    -Verified tests are failing

---
+++ RomanToDecimalConverter/RomanToDecimalConverter/RomanToDecimalConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanToDecimalConversion
+{
+    public class RomanToDecimalConverter
+    {
+        /// <summary>
+        /// Gets the decimal value for roman symbol
+        /// </summary>
+        /// <param name="romanSymbol">roman symbol</param>
+        /// <returns>decimal value of roman symbol</returns>
+        private static int GetValueFromRomanSymbol(char romanSymbol)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Converts Roman numerals to decimal
+        /// </summary>
+        /// <param name="romanStr">string for the roman numerals</param>
+        /// <returns>decimal value for the given roman numerals</returns>
+        public static int RomanToDecimal(string romanStr)
+        {
+            int result = 0;
+            return result;
+        }
+    
+    }
+}
