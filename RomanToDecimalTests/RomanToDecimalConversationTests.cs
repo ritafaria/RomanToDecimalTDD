24ca7df6803c117619889894c2ac10468af01de7
ritafaria <rita.faria@live.com>
2016-09-06 01:27:29 -0700

    Add files via upload
    Initial test project for roman to decimal convertion with TDD

---
+++ RomanToDecimalTests/RomanToDecimalConversationTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanToDecimalTests
+{
+    [TestClass]
+    public class RomanToDecimalConversationTests
+    {
+        [TestMethod]
+        public void VerifyDecimalValueForSingleRomanSymbol()
+        {
+            string romanStr = "V";
+
+            int val = RomanToDecimalConverter.RomanToDecimal(romanStr);
+            Assert.AreEqual(5, val);
+
+            romanStr = "C";
+            val = RomanToDecimalConverter.RomanToDecimal(romanStr);
+            Assert.AreEqual(100, val);
+        }
+
+        [TestMethod]
+        public void VerifyDecimalValueForRepeatingRomanSymbol()
+        {
+            string romanStr = "II";
+
+            int val = RomanToDecimalConverter.RomanToDecimal(romanStr);
+            Assert.AreEqual(2, val);
+
+            romanStr = "XXX";
+            val = RomanToDecimalConverter.RomanToDecimal(romanStr);
+            Assert.AreEqual(30, val);
+
+            romanStr = "CC";
+            val = RomanToDecimalConverter.RomanToDecimal(romanStr);
+            Assert.AreEqual(200, val);
+        }
+
+        [TestMethod]
+        public void VerifyDecimalValueForRomansubtractivenotation()
+        {
+            string romanStr = "IV";
+
+            int val = RomanToDecimalConverter.RomanToDecimal(romanStr);
+            Assert.AreEqual(4, val);
+
+            romanStr = "CD";
+            val = RomanToDecimalConverter.RomanToDecimal(romanStr);
+            Assert.AreEqual(400, val);
+        }
+    }
+}
